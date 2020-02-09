2e874fb10fb62260e0e7db2b0452dea5dccf9980
wisemk <wisemk79@gmail.com>
2020-02-09 10:58:50 +0900

    study

--- JSP/ex20_CommandPattern/src/com/javalec/ex/MembersAllService.java
+++ JSP/ex20_CommandPattern/src/com/javalec/ex/MembersAllService.java
@@ -11,7 +11,7 @@ public class MembersAllService implements Service {
 		// TODO Auto-generated constructor stub
 	}
 
-	@Override //<-- Service 인터페이스에서 오버라이드된 객체   memberdto를 사용하는 어레이리스트를 반환형으로 해줌 
+	@Override //<-- Service 인터페이스에서 오버라이드된 객체   memberdto를 사용하는 어레이리스트를  반환형으로 해줌 
 	public ArrayList<MemberDto> execute(HttpServletRequest request, HttpServletResponse response) {//이것의 execute를 이용하여 dao에 접근하여 메소드 호출 
 		// TODO Auto-generated method stub
 		
